57b05ac43c698bf542844e988389df32440b65ae
Arthur <baginian.artur@yandex.ru>
2025-01-03 17:26:59 +0300

    add emailvalidator + some schemas changes

--- smart_fridge/lib/schemas/auth.py
+++ smart_fridge/lib/schemas/auth.py
@@ -1,6 +1,21 @@
-from uuid import UUID
+from datetime import datetime, timezone
+from uuid import UUID, uuid4
 
+from jwt import encode as jwt_encode
+
+from . import fields as f
 from .abc import BaseSchema
+from .user import USER_EMAIL, USER_PASSWORD
+
+
+TOKEN = f.BaseField(
+    description="JSON Web Token.",
+    examples=[jwt_encode({"sub": str(uuid4()), "exp": datetime.now(timezone.utc)}, "SECRET_KEY")],
+)
+TOKEN_EXPIRATION = f.BaseField(description="Token expiration in minutes.", ge=5, le=525600, examples=[43800])
+FINGERPRINT = f.BaseField(
+    description="Fingerprint.", min_length=32, max_length=64, examples=["f1b7e156414663c4b81fbadadedcf01f"]
+)
 
 
 class TokenRedisData(BaseSchema):
@@ -10,12 +25,12 @@ class TokenRedisData(BaseSchema):
 
 
 class TokenCreateSchema(BaseSchema):
-    email: str
-    password: str
+    email: str = USER_EMAIL
+    password: str = USER_PASSWORD
 
 
 class TokenSchema(BaseSchema):
-    access_token: str
-    refresh_token: str
-    access_token_expires_in: int
-    refresh_token_expires_in: int
+    access_token: str = TOKEN
+    refresh_token: str = TOKEN
+    access_token_expires_in: int = TOKEN_EXPIRATION
+    refresh_token_expires_in: int = TOKEN_EXPIRATION
