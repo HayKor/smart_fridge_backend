240ccaa2e258fc467a9d3ba367de8b31e8657612
Arthur <baginian.artur@yandex.ru>
2025-01-03 17:08:24 +0300

    add fields for products

--- smart_fridge/lib/schemas/product.py
+++ smart_fridge/lib/schemas/product.py
@@ -1,28 +1,36 @@
 from datetime import datetime
 
+from . import fields as f
 from .abc import BaseSchema
+from .product_type import PRODUCT_TYPE_ID
+
+
+PRODUCT_ID = f.ID(prefix="Product ID.")
+PRODUCT_AMOUNT = f.BaseField(description="Product amount based off of its accounting type", examples=[3])
+MANUFACTURE_AT = f.DATETIME(description="Product manufacturing datetime")
+OPENED_AT = f.DATETIME(description="Product opening datetime")
 
 
 class BaseProductSchema(BaseSchema):
-    amount: int
+    amount: int = PRODUCT_AMOUNT
 
 
 class ProductCreateSchema(BaseProductSchema):
-    product_type_id: int
+    product_type_id: int = PRODUCT_TYPE_ID
     owner_id: int
-    manufacture_at: datetime | None = None
+    manufacture_at: datetime = MANUFACTURE_AT
 
 
 class ProductUpdateSchema(ProductCreateSchema):
     pass
 
 
-class ProductPatchSchema(BaseSchema):
-    amount: int | None = None
-    manufacture_at: datetime | None = None
-    opened_at: datetime | None
+class ProductPatchSchema(ProductCreateSchema):
+    amount: int = PRODUCT_AMOUNT(default=None)
+    manufacture_at: datetime = MANUFACTURE_AT(default=None)
+    opened_at: datetime | None = OPENED_AT(default=None)
 
 
 class ProductSchema(ProductCreateSchema):
-    id: int
-    opened_at: datetime | None = None
+    id: int = PRODUCT_ID
+    opened_at: datetime | None = OPENED_AT(default=None)
