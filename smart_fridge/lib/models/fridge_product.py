71043f3a1f9e69e31fe55addcb58b92c4d4ba592
Arthur <baginian.artur@yandex.ru>
2025-01-09 21:07:46 +0300

    fix(fridge_product): fix some columns

--- smart_fridge/lib/models/fridge_product.py
+++ smart_fridge/lib/models/fridge_product.py
@@ -9,13 +9,15 @@ from .abc import AbstractModel
 
 if TYPE_CHECKING:
     from .fridge import FridgeModel
+    from .product import ProductModel
 
 
 class FridgeProductModel(AbstractModel):
     __tablename__ = "fridge_products"
     id: Mapped[int] = mapped_column("id", Integer(), primary_key=True, autoincrement=True)
     fridge_id: Mapped[int] = mapped_column("fridge_id", ForeignKey("fridges.id"), nullable=False)
-    product_type_id: Mapped[int] = mapped_column("product_type_id", ForeignKey("product_types.id"), nullable=False)
+    product_id: Mapped[int] = mapped_column("product_id", ForeignKey("products.id"), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
     deleted_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), nullable=True)
     fridge: Mapped["FridgeModel"] = relationship("FridgeModel", back_populates="fridge_products")
+    product: Mapped["ProductModel"] = relationship("ProductModel")
