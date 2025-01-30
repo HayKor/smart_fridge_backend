7b9a8ead3cc26ab659f68cb14c70eef9f4c84bfb
Arthur Baginian <63199429+HayKor@users.noreply.github.com>
2025-01-30 20:28:22 +0300

    feat(cli): add CLI & new Dockerfile (#8)
    * feat(cli): add CLI & new Dockerfile

---
+++ smart_fridge/__main__.py
@@ -0,0 +1,10 @@
+from .cli import app
+
+
+def main() -> None:
+    """Call the cli."""
+    app()
+
+
+if __name__ == "__main__":
+    main()
---
+++ smart_fridge/cli/cli.py
@@ -0,0 +1,38 @@
+from typing import Annotated
+
+import typer
+import uvicorn
+
+
+app = typer.Typer()
+
+
+@app.command()
+def run(
+    host: Annotated[str, typer.Option("--host", "-h")] = "0.0.0.0",
+    port: Annotated[int, typer.Option("--port", "-p")] = 8080,
+    reload: Annotated[bool, typer.Option()] = False,
+    workers: Annotated[int, typer.Option("--workers", "-w")] = 1,
+) -> None:
+    """Run the prod app."""
+    uvicorn.run(
+        "smart_fridge.app:app",
+        host=host,
+        port=port,
+        reload=reload,
+        workers=workers,
+        factory=True,
+    )
+
+
+@app.command()
+def dev() -> None:
+    """Run the local dev app."""
+    uvicorn.run(
+        "smart_fridge.app:app",
+        host="127.0.0.1",
+        port=8080,
+        reload=True,
+        workers=1,
+        factory=True,
+    )
